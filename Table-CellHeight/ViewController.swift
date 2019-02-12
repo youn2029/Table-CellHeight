0606db9a6c7fce030399fde36718f3fad184f86d
윤성호 <youn2029@naver.com>
2019-02-12 15:40:01 +0900

    Initial Commit

---
+++ Table-CellHeight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table-CellHeight
+//
+//  Created by 윤성호 on 12/02/2019.
+//  Copyright © 2019 윤성호. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
